e5f7436b0b6922b60159f38263a32f767c6baca6
Dianna <dianna.carmalt@pomeroy.com>
2018-07-09 14:36:33 -0400

    comments

--- Githubby/ViewController.m
+++ Githubby/ViewController.m
@@ -6,24 +6,31 @@
 //  Copyright © 2018 Dianna Carmalt. All rights reserved.
 //
 
+//Comments by Dianna
+
 #import "ViewController.h"
 
 @interface ViewController ()
 
+// Comments by Dianna
+
 @end
 
 @implementation ViewController
+
+// Comments by Dianna
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+//Comments by Dianna
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+//Comments by Dianna
 
 @end
